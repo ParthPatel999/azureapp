aadd37ac9451b7739f3cc7b85aacdfc7cb7b9613
parthpatel <parth.patel@nl.abnamro.com>
2023-06-21 14:47:37 +0200

    adding logging

--- src/main/java/com/example/SampleSpringBoot/HelloController.java
+++ src/main/java/com/example/SampleSpringBoot/HelloController.java
@@ -6,14 +6,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.azure.spring.cloud.feature.management.FeatureManager;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@Slf4j
 public class HelloController {
 
   @Autowired FeatureManager featureManager;
+  
+  ObjectMapper map = new ObjectMapper();
 
   @GetMapping("/welcome")
   public String testAppConfiguration() throws JsonProcessingException {
+    
+    log.info("featureManager::" + map.writeValueAsString(featureManager));
+    log.info(
+        "featureManager getAllFeatureNames::"
+            + map.writeValueAsString(featureManager.getAllFeatureNames()));
     
     if (featureManager.isEnabledAsync("test").block()) {
       return "Welcome!";
