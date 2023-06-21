020066c0dbb7d6de6616ef49e67b3d316fdee584
parthpatel <parth.patel@nl.abnamro.com>
2023-06-21 14:28:30 +0200

    adding app conf configuration

--- src/main/java/com/example/SampleSpringBoot/HelloController.java
+++ src/main/java/com/example/SampleSpringBoot/HelloController.java
@@ -1,12 +1,24 @@
 package com.example.SampleSpringBoot;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.azure.spring.cloud.feature.management.FeatureManager;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 @RestController
 public class HelloController {
-  @RequestMapping("/getUser")
-  public String hello() {
-    return "Hello Parth";
+
+  @Autowired FeatureManager featureManager;
+
+  @GetMapping("/welcome")
+  public String testAppConfiguration() throws JsonProcessingException {
+    
+    if (featureManager.isEnabledAsync("test").block()) {
+      return "Welcome!";
+    } else {
+      return "Not Welcome!!!";
+    }
   }
 }
